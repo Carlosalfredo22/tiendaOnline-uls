fe50d2d820ec1cb913524ebff5b2aa2510e5ab76
Carlosalfredo22 <gabriela472151@gmail.com>
2025-03-17 17:15:53 -0600

    Gabriela
    detalles pedidos

--- database/migrations/2025_03_17_224352_detallespedidos.php
+++ database/migrations/2025_03_17_224352_detallespedidos.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void
     {
-        //tabla detalles pedidos  gabriela
+        //tabla detalles pedidos-gabriela
         Schema::create('detallespedidos', function (Blueprint $table) {
             $table->id('iddetallespedidos');
             $table->integer('cantidad');
