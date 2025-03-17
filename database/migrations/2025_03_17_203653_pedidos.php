b1978ee38e46faab29d6cce02290fa1cf056b181
Maricela G. Sánchez <149551151+MaricelaS20@users.noreply.github.com>
2025-03-17 15:22:51 -0600

    Maricela
    tabla pedidos

--- database/migrations/2025_03_17_203653_pedidos.php
+++ database/migrations/2025_03_17_203653_pedidos.php
@@ -4,21 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+return new class extends Migration {
+
+    public function up()
     {
-        //tabla pedidos Maricela.
+        // Crear tabla pedidos Maricela
+        Schema::create('pedidos', function (Blueprint $table) {
+            $table->id('idpedidos'); // Llave primaria
+            $table->date('fecha_de_pedido'); // Fecha del pedido
+            $table->enum('estado', ['pendiente', 'enviado', 'otros']); // Estado del pedido
+            $table->foreignId('idusuario')->constrained('usuario')->onDelete('cascade'); // Llave foránea de usuario
+            $table->foreignId('idmetodopago')->constrained('metodopago')->onDelete('cascade'); // Llave foránea de método de pago
+            $table->timestamps(); // Campos created_at y updated_at
+        });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
-        //
+        Schema::dropIfExists('pedidos');
     }
 };
